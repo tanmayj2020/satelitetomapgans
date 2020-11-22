2c2390b2270b6e4fa85a86e69e769e3a1a099d6e
Tanmay Jain <55263849+tanmayj2020@users.noreply.github.com>
2020-11-22 23:24:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,266 @@
+#Necessary imports of libraries
+import tensorflow as tf
+
+from os import listdir
+from numpy import asarray
+from numpy import vstack
+import numpy as np
+from keras.preprocessing.image import img_to_array , load_img
+from numpy import savez_compressed
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.layers import Conv2D , LeakyReLU , BatchNormalization , Dropout ,Concatenate , Conv2DTranspose , Activation
+from tensorflow.keras import Model , Input 
+from keras.optimizers import Adam
+from random import randint
+from numpy import load
+from matplotlib import pyplot
+from keras.backend import clear_session
+clear_session()
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.compat.v1.Session(config=config)
+
+
+# define the discriminator model
+def define_discriminator(image_shape):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # source image input
+    in_src_image = Input(shape=image_shape)
+    # target image input
+    in_target_image = Input(shape=image_shape)
+    # concatenate images channel-wise
+    merged = Concatenate()([in_src_image, in_target_image])
+    # C64
+    d = Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C128
+    d = Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C256
+    d = Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C512
+    d = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # second last output layer
+    d = Conv2D(512, (4,4), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # patch output
+    d = Conv2D(1, (4,4), padding='same', kernel_initializer=init)(d)
+    patch_out = Activation('sigmoid')(d)
+    # define model
+    model = Model([in_src_image, in_target_image], patch_out)
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, loss_weights=[0.5])
+    return model
+
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+    return result
+
+def downsample(filters, size, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+    tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',kernel_initializer=initializer, use_bias=False))
+
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
+def define_generator(image_shape):
+    inputs = tf.keras.layers.Input(shape=image_shape)
+
+    down_stack = [
+    downsample(64, 4, apply_batchnorm=False), # (bs, 128, 128, 64)
+    downsample(128, 4), # (bs, 64, 64, 128)
+    downsample(256, 4), # (bs, 32, 32, 256)
+    downsample(512, 4), # (bs, 16, 16, 512)
+    downsample(512, 4), # (bs, 8, 8, 512)
+    downsample(512, 4), # (bs, 4, 4, 512)
+    downsample(512, 4), # (bs, 2, 2, 512)
+    downsample(512, 4), # (bs, 1, 1, 512)
+    ]
+
+    up_stack = [
+    upsample(512, 4, apply_dropout=True), # (bs, 2, 2, 1024)
+    upsample(512, 4, apply_dropout=True), # (bs, 4, 4, 1024)
+    upsample(512, 4, apply_dropout=True), # (bs, 8, 8, 1024)
+    upsample(512, 4), # (bs, 16, 16, 1024)
+    upsample(256, 4), # (bs, 32, 32, 512)
+    upsample(128, 4), # (bs, 64, 64, 256)
+    upsample(64, 4), # (bs, 128, 128, 128)
+    ]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = tf.keras.layers.Conv2DTranspose(3, 4,strides=2,padding='same',kernel_initializer=initializer,activation='tanh') # (bs, 256, 256, 3)
+
+    
+    
+    x = inputs
+
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+
+    skips = reversed(skips[:-1])
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = tf.keras.layers.Concatenate()([x, skip])
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
+ 
+# define the combined generator and discriminator model, for updating the generator
+def define_gan(g_model, d_model, image_shape):
+    # make weights in the discriminator not trainable
+    d_model.trainable = False
+    # define the source image
+    in_src = Input(shape=image_shape)
+    # connect the source image to the generator input
+    gen_out = g_model(in_src)
+    # connect the source input and generator output to the discriminator input
+    dis_out = d_model([in_src, gen_out])
+    # src image as input, generated image and classification output
+    model = Model(in_src, [dis_out, gen_out])
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss=['binary_crossentropy', 'mae'], optimizer=opt, loss_weights=[1,100])
+    return model
+ 
+# load and prepare training images
+def load_real_samples(filename):
+    # load compressed arrays
+    data = load(filename)
+    # unpack arrays
+    X1, X2 = data['arr_0'], data['arr_1']
+    # scale from [0,255] to [-1,1]
+    X1 = (X1 - 127.5) / 127.5
+    X2 = (X2 - 127.5) / 127.5
+    return [X1, X2]
+ 
+# select a batch of random samples, returns images and target
+def generate_real_samples(dataset, n_samples, patch_shape):
+    # unpack dataset
+    trainA, trainB = dataset
+    # choose random instances
+    ix = np.random.randint(0, trainA.shape[0], n_samples)
+    # retrieve selected images
+    X1, X2 = trainA[ix], trainB[ix]
+    # generate 'real' class labels (1)
+    y = np.ones((n_samples, patch_shape, patch_shape, 1))
+    return [X1, X2], y
+ 
+# generate a batch of images, returns images and targets
+def generate_fake_samples(g_model, samples, patch_shape):
+    # generate fake instance
+    X = g_model.predict(samples)
+    # create 'fake' class labels (0)
+    y = np.zeros((len(X), patch_shape, patch_shape, 1))
+    return X, y
+ 
+# generate samples and save as a plot and save the model
+def summarize_performance(step, g_model, dataset, n_samples=3):
+    # select a sample of input images
+    [X_realA, X_realB], _ = generate_real_samples(dataset, n_samples, 1)
+    # generate a batch of fake samples
+    X_fakeB, _ = generate_fake_samples(g_model, X_realA, 1)
+    # scale all pixels from [-1,1] to [0,1]
+    X_realA = (X_realA + 1) / 2.0
+    X_realB = (X_realB + 1) / 2.0
+    X_fakeB = (X_fakeB + 1) / 2.0
+    # plot real source images
+    for i in range(n_samples):
+        pyplot.subplot(3, n_samples, 1 + i)
+        pyplot.axis('off')
+        pyplot.imshow(X_realA[i])
+    # plot generated target image
+    for i in range(n_samples):
+        pyplot.subplot(3, n_samples, 1 + n_samples + i)
+        pyplot.axis('off')
+        pyplot.imshow(X_fakeB[i])
+    # plot real target image
+    for i in range(n_samples):
+        pyplot.subplot(3, n_samples, 1 + n_samples*2 + i)
+        pyplot.axis('off')
+        pyplot.imshow(X_realB[i])
+    # save plot to file
+    filename1 = 'plot_%06d.png' % (step+1)
+    pyplot.savefig(filename1)
+    pyplot.close()
+    # save the generator model
+    filename2 = 'model_%06d.h5' % (step+1)
+    g_model.save(filename2)
+    print('>Saved: %s and %s' % (filename1, filename2))
+ 
+# train pix2pix models
+def train(d_model, g_model, gan_model, dataset, n_epochs=100, n_batch=1):
+    # determine the output square shape of the discriminator
+    n_patch = d_model.output_shape[1]
+    # unpack dataset
+    trainA, trainB = dataset
+    # calculate the number of batches per training epoch
+    bat_per_epo = int(len(trainA) / n_batch)
+    # calculate the number of training iterations
+    n_steps = bat_per_epo * n_epochs
+    # manually enumerate epochs
+    for i in range(n_steps):
+        # select a batch of real samples
+        [X_realA, X_realB], y_real = generate_real_samples(dataset, n_batch, n_patch)
+        # generate a batch of fake samples
+        X_fakeB, y_fake = generate_fake_samples(g_model, X_realA, n_patch)
+        # update discriminator for real samples
+        d_loss1 = d_model.train_on_batch([X_realA, X_realB], y_real)
+        # update discriminator for generated samples
+        d_loss2 = d_model.train_on_batch([X_realA, X_fakeB], y_fake)
+        # update the generator
+        g_loss, _, _ = gan_model.train_on_batch(X_realA, [y_real, X_realB])
+        # summarize performance
+        print('>%d, d1[%.3f] d2[%.3f] g[%.3f]' % (i+1, d_loss1, d_loss2, g_loss))
+        # summarize model performance
+        if (i+1) % (bat_per_epo * 10) == 0:
+            summarize_performance(i, g_model, dataset)
+ 
+# load image data
+dataset = load_real_samples('maps_256.npz')
+print('Loaded', dataset[0].shape, dataset[1].shape)
+# define input shape based on the loaded dataset
+image_shape = dataset[0].shape[1:]
+# define the models
+d_model = define_discriminator(image_shape)
+g_model = define_generator(image_shape)
+# define the composite model
+gan_model = define_gan(g_model, d_model, image_shape)
+# train model
+train(d_model, g_model, gan_model, dataset)
